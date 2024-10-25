3d148797484c911e15863258bff9ef11bd9a53f9
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-25 11:29:39 -0600

    Refactor database schema and add 'keeps' table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,20 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) UNIQUE COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) UNIQUE COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE keeps (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    img VARCHAR(1000) NOT NULL,
+    views INT NOT NULL,
+    creatorId VARCHAR(255) NOT NULL,
+    Foreign Key (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+)

4fc562c0b1b5a018dbfda5966513649e396f340a
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-25 11:06:41 -0600

    Updated .env and env.js environment files

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-judurlmgk3qh4irk.us.auth0.com'
+export const clientId = 'erqGjlH8ucqZE4o6Q1qfa2d6SiOLwOOP'
+export const audience = 'https://auth.speedwagonfoundation.com'

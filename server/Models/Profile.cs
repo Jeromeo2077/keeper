c7e04dff34a02f9050d5125a9caf693b22b45585
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-25 11:42:27 -0600

    Refactor Account to inherit from new Profile model; Created new Keep model

---
+++ server/Models/Profile.cs
@@ -0,0 +1,11 @@
+namespace keeper.Models;
+
+public class Profile
+{
+  public string Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public string Name { get; set; }
+  public string Picture { get; set; }
+  public string CoverImg { get; set; }
+}

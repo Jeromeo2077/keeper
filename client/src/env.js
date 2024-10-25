7c2818df780b05dc728a37500cbd56ea1f3bd77d
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-25 12:55:14 -0600

    Update baseURL in env.js to use http://localhost:8080 for development

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:7045' : ''
+export const baseURL = dev ? 'http://localhost:8080' : ''
 export const useSockets = false
 export const domain = 'dev-judurlmgk3qh4irk.us.auth0.com'
 export const clientId = 'erqGjlH8ucqZE4o6Q1qfa2d6SiOLwOOP'

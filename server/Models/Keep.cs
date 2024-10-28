1fd86145aff44a5038490eb617762acbdc065422
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-28 14:01:56 -0600

    Refactor Keep model to remove unnecessary Required attributes

--- server/Models/Keep.cs
+++ server/Models/Keep.cs
@@ -4,31 +4,24 @@ namespace keeper.Models;
 
 public class Keep
 {
-  [Required]
   public int Id { get; set; }
 
-  [Required]
   public DateTime CreatedAt { get; set; }
 
-  [Required]
   public DateTime UpdatedAt { get; set; }
 
   [MaxLength(255)]
-  [Required]
   public string Name { get; set; }
 
   [MaxLength(1000)]
-  [Required]
   public string Description { get; set; }
 
   [MaxLength(1000)]
   [Url]
   public string Img { get; set; }
 
-  [Required]
   public int Views { get; set; } = 0;
 
-  [Required]
   public int Kept { get; set; } = 0;
 
   public string CreatorId { get; set; }

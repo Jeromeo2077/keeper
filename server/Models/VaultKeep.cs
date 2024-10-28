2f65de3b3de9779756758d853ce496866a7017bf
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-28 11:16:41 -0600

    Refactor VaultKeep model to remove unnecessary Required attributes

--- server/Models/VaultKeep.cs
+++ server/Models/VaultKeep.cs
@@ -4,13 +4,10 @@ namespace keeper.Models;
 
 public class VaultKeep
 {
-  [Required]
   public int Id { get; set; }
 
-  [Required]
   public int VaultId { get; set; }
 
-  [Required]
   public int KeepId { get; set; }
   public string CreatorId { get; set; }
   public Profile Creator { get; set; }

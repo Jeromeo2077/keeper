d30f78b92679a044422e26c70654f05deff06e3c
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-28 20:40:50 -0600

    Refactor font styles and update page title in index.html and HomePage.vue

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -3,9 +3,14 @@
 </script>
 
 <template>
-  <div>
-    <h1>Keeper</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-Left">Keeper</h1>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <style scoped lang="scss"></style>

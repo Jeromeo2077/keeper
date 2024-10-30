5ca27b5c486cf14a4962d0bbe49d759cd353ee91
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-30 14:42:29 -0600

    Refactor ProfilePicture.vue component to display a profile picture with rounded border and fixed dimensions

---
+++ client/src/components/globals/ProfilePicture.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Profile } from '@/models/Profile.js';
+
+defineProps({
+  profile: { type: Profile, required: true }
+})
+</script>
+
+
+<template>
+  <img :src="profile.picture" :alt="profile.name" :title="profile.name">
+</template>
+
+
+<style lang="scss" scoped>
+img {
+  width: 50px;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  object-fit: cover;
+}
+</style>

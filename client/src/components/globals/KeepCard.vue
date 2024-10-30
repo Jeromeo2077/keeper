59af51a3a66e6f41c354c9ba26e7eed8d7323a2a
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-30 17:18:41 -0600

    Add KeepDetailsModal component and integrate it into KeepCard for displaying keep details

--- client/src/components/globals/KeepCard.vue
+++ client/src/components/globals/KeepCard.vue
@@ -1,18 +1,10 @@
 <script setup>
 import { Keep } from '@/models/Keep.js';
 import ProfilePicture from './ProfilePicture.vue';
-import { computed, onMounted } from 'vue';
-import { AppState } from '@/AppState.js';
 import { keepsService } from '@/services/KeepsService.js';
-import { useRoute } from 'vue-router';
 import Pop from '@/utils/Pop.js';
 import { logger } from '@/utils/Logger.js';
-
-
-const activeKeep = computed(() => {
-  return AppState.activeKeep;
-});
-
+import KeepDetailsModal from './KeepDetailsModal.vue';
 
 defineProps({ keep: { type: Keep, required: true } });
 
@@ -31,31 +23,17 @@ async function getKeepDetailsById(keepId) {
 
 <template>
   <div type="button" class="keep-card btn" @click="getKeepDetailsById(keep.id)" data-bs-toggle="modal"
-    data-bs-target="#KeepDetails">
+    data-bs-target="#KeepDetailsModal">
     <img :src="keep.img" :alt="keep.name" class="img-fluid keep-img rounded border border-3 shadow">
     <h3>{{ keep.name }}</h3>
     <span>
       <ProfilePicture :profile="keep.creator" />
     </span>
+
+    <KeepDetailsModal />
   </div>
 
-  <div class="modal fade" id="KeepDetails" tabindex="-1" aria-labelledby="Keep Details" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Keep Name</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>
+
 </template>
 
 
---
+++ client/src/components/globals/KeepDetailsModal.vue
@@ -0,0 +1,41 @@
+.
+<script setup>
+import { AppState } from '@/AppState.js';
+import { computed } from 'vue';
+
+
+const activeKeep = computed(() => {
+  return AppState.activeKeep;
+});
+
+</script>
+
+
+<template>
+
+  <div class="modal fade" id="KeepDetailsModal" tabindex="-1" aria-labelledby="Keep Details" aria-hidden="true">
+    <div class="modal-dialog">
+      <div v-if="activeKeep" class="modal-content">
+        <div class="modal-header">
+          <div>
+            <img :src="activeKeep.img" :alt="activeKeep.name" class="img-fluid rounded border border-3 shadow">
+          </div>
+          <div>
+            <h1 class="modal-title fs-5" id="active_keep_name">{{ activeKeep.name }}</h1>
+          </div>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped></style>
